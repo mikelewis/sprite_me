98e5b14c0233900a607adeb97f88620c3ec819d3
Mike Lewis <ft.mikelewis@gmail.com>
2012-05-13 20:02:34 -0700

    Don't need CGI anymore

--- lib/sprite_me.rb
+++ lib/sprite_me.rb
@@ -2,8 +2,8 @@ require "em-synchrony"
 require 'em-synchrony/em-http'
 require 'goliath'
 require 'yajl'
-require 'cgi'
 require 'base64'
+
 require 'sprite_me/image'
 require 'sprite_me/downloader'
 require 'sprite_me/json_params'
